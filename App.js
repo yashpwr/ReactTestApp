092254ad831196cf60f84727cbd92ae335913737
yash pawar <45911343+yashpwr@users.noreply.github.com>
2020-12-19 20:25:54 +0530

    Working with Dimension and Oriantation

--- App.js
+++ App.js
@@ -1,74 +1,30 @@
 import React from 'react';
-import { StyleSheet, 
-        Text, 
-        TouchableHighlight, 
-        Image, 
-        Alert, 
-        SafeAreaView, 
-        Button, 
-        Platform,
-        StatusBar } from 'react-native';
+import { StyleSheet, SafeAreaView, Platform, StatusBar, View, Dimensions } from 'react-native';
+import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
 
-const handlepress = () => console.log("text pressed");
+        export default function App() {
+          //console.log(useDeviceOrientation());
+          //console.log(Dimensions.get("screen"));
 
-const App1 = () => {
+          const {landscape} = useDeviceOrientation();
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text onPress={handlepress}>Hello <Text style={styles.innerText}>World </Text></Text>
-      <TouchableHighlight onPress={() => console.log('Image tapepd')}>
-      <Image 
-      blurRadius={1}
-      fadeDuration={1000}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300"
-        }} />
-      </TouchableHighlight>
-    </SafeAreaView>
-  );
-}
-
-const containerStyle = {color: "orange"};
-const App2 = () => {
-  return(
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <Button 
-      color="orange"
-      title="click Me" 
-      onPress={()=> 
-        Alert.alert("My Tittle", "My Message", [ 
-          {text: "Yes", onPress: ()=> console.log('Cliked Yes')}, 
-          {text: "No", onPress: ()=> console.log('Cliked No')}, 
-          ]) 
-        }
-      />
-      
-    </SafeAreaView>
-  );
-}
+          return(
+            <SafeAreaView style={styles.container}>
+              <View style={{
+                backgroundColor: "dodgerblue",
+                width: "100%",
+                height: landscape ? '100%' : '50%',
+              }}></View>
+            </SafeAreaView>
+          );
+        };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'dodgerblue',
+    //flex: 1,
+    //backgroundColor: 'dodgerblue',
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     //alignItems: 'center',
     //justifyContent: 'center',
   },
-  innerText:{
-    color: 'red'
-  }
 });
-
-const layout = () => {
-  return(
-    <SafeAreaView style={styles.container}>
-
-    </SafeAreaView>
-  );
-};
-
-//export default App1;
-export default App2;
