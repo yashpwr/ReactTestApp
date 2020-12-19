5d0ce6f0c4bec58c703682e5fe165fad19466ec4
yashpwr <yashdvn@gmail.com>
2020-12-19 18:19:16 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, 
+        Text, 
+        TouchableHighlight, 
+        Image, 
+        Alert, 
+        SafeAreaView, 
+        Button, 
+        Platform,
+        StatusBar } from 'react-native';
 
-export default function App() {
+const handlepress = () => console.log("text pressed");
+
+const App1 = () => {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlepress}>Hello <Text style={styles.innerText}>World </Text></Text>
+      <TouchableHighlight onPress={() => console.log('Image tapepd')}>
+      <Image 
+      blurRadius={1}
+      fadeDuration={1000}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300"
+        }} />
+      </TouchableHighlight>
+    </SafeAreaView>
+  );
+}
+
+const containerStyle = {color: "orange"};
+const App2 = () => {
+  return(
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Button 
+      color="orange"
+      title="click Me" 
+      onPress={()=> 
+        Alert.alert("My Tittle", "My Message", [ 
+          {text: "Yes", onPress: ()=> console.log('Cliked Yes')}, 
+          {text: "No", onPress: ()=> console.log('Cliked No')}, 
+          ]) 
+        }
+      />
+      
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
+  innerText:{
+    color: 'red'
+  }
 });
+
+const layout = () => {
+  return(
+    <SafeAreaView style={styles.container}>
+
+    </SafeAreaView>
+  );
+};
+
+//export default App1;
+export default App2;
