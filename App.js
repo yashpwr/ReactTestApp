69b7150e44b5dd65f0120196bbb4096923239d4f
yash pawar <45911343+yashpwr@users.noreply.github.com>
2020-12-19 21:17:00 +0530

    Flexbox- flexWrap and alignContent

--- App.js
+++ App.js
@@ -9,24 +9,37 @@ import { StyleSheet, SafeAreaView, Platform, StatusBar, View, Dimensions } from 
               flex: 1,
               flexDirection: "row", //Horizontal
               justifyContent: "center", //Main
-              alignItems: "center"
+              alignItems: "center",
+              flexWrap: 'wrap',
+              alignContent: "center",
             }}>
 
             <View style={{
               backgroundColor: "dodgerblue",
               width: 100,
               height: 300,
-              alignSelf: "flex-start",
               //flex: 1,
             }}/>
             <View style={{
               backgroundColor: "gold",
               width: 100,
-              height: 200,
+              height: 100,
               //flex: 1,
             }}/>
             <View style={{
               backgroundColor: "tomato",
+              width: 100,
+              height: 100,
+              //flex: 1,
+            }}/>
+            <View style={{
+              backgroundColor: "grey",
+              width: 100,
+              height: 100,
+              //flex: 1,
+            }}/>
+            <View style={{
+              backgroundColor: "green",
               width: 100,
               height: 100,
               //flex: 1,
